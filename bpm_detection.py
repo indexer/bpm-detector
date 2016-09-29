ebc58b3e891845d5a80c2e5fa3d9425b6c4abb35
Indexer <yemonkyaw007@gmail.com>
2016-09-29 12:36:17 +0630

    fix double converted missing error

--- bpm_detection.py
+++ bpm_detection.py
@@ -2,47 +2,50 @@ import wave, array, math, time, argparse, sys
 import numpy, pywt
 from scipy import signal
 import pdb
+import os
 from pydub import AudioSegment
 import matplotlib.pyplot as plt
 from pydub import AudioSegment
-from mutagen.id3 import ID3, TPE1,TBPM,ID3NoHeaderError
+from mutagen.id3 import ID3, TPE1,TBPM ,ID3NoHeaderError
+
+
+
 
 def read_wav(filename):
 
     #open file, get metadata for audio
     try:
-        wsound = AudioSegment.from_mp3(filename)
-        wsound.export(filename, format="wav")
 
-        wave_file = wave.open(filename,'rb')
-	
-    except IOError:
-        print ("File Not Found")
+           sound = AudioSegment.from_mp3(filename)
+	   sound.export(filename+"converted", format="wav")
+           wf = wave.open(filename+"converted",'rb')
+
+        
+    except IOError, e:
+        print e
         return
 
-
-    nsamps = wave_file.getnframes();
+    # typ = choose_type( wf.getsampwidth() ) #TODO: implement choose_type
+    nsamps = wf.getnframes();
     assert(nsamps > 0);
 
-    frame_per_second = wave_file.getframerate()
-    assert(frame_per_second > 0)
+    fs = wf.getframerate()
+    assert(fs > 0)
 
     # read entire file and make into an array
-    samps = list(array.array('i',wave_file.readframes(nsamps)))
+    samps = list(array.array('i',wf.readframes(nsamps)))
     #print 'Read', nsamps,'samples from', filename
     try:
         assert(nsamps == len(samps))
-    except AssertionError :
-        print  (nsamps, "not equal to", len(samps))
+    except AssertionError, e:
+        print  nsamps, "not equal to", len(samps)
     
-    return samps, frame_per_second
+    return samps, fs
     
-
+# print an error when no data can be found
 def no_audio_data():
-    print ("No audio data for sample, skipping...")
+    print "No audio data for sample, skipping..."
     return None, None
-
-
     
 # simple peak detection
 def peak_detect(data):
@@ -52,17 +55,17 @@ def peak_detect(data):
         peak_ndx = numpy.where(data==-max_val)
     return peak_ndx
     
-def bpm_detector(data,frame_per_second):
+def bpm_detector(data,fs):
     cA = [] 
     cD = []
     correl = []
     cD_sum = []
     levels = 4
     max_decimation = 2**(levels-1);
-    min_ndx = 60./ 220 * (frame_per_second/max_decimation)
-    max_ndx = 60./ 40 * (frame_per_second/max_decimation)
+    min_ndx = 60./ 220 * (fs/max_decimation)
+    max_ndx = 60./ 40 * (fs/max_decimation)
     
-    for loop in range(0,int(levels)):
+    for loop in range(0,levels):
         cD = []
         # 1) DWT
         if loop == 0:
@@ -103,8 +106,8 @@ def bpm_detector(data,frame_per_second):
         return no_audio_data()
         
     peak_ndx_adjusted = peak_ndx[0]+min_ndx;
-    bpm = 60./ peak_ndx_adjusted * (frame_per_second/max_decimation)
-    print (bpm)
+    bpm = 60./ peak_ndx_adjusted * (fs/max_decimation)
+    print bpm
     return bpm,correl
     
     
@@ -116,27 +119,28 @@ if __name__ == '__main__':
                    help='size of the the window (seconds) that will be scanned to determine the bpm.  Typically less than 10 seconds. [3]')
 
     args = parser.parse_args()
-    samps,frame_per_second = read_wav(args.filename)
+    samps,fs = read_wav(args.filename)
     
     data = []
     correl=[]
     bpm = 0
     n=0;
     nsamps = len(samps)
-    window_samps = int(args.window*frame_per_second)         
+    window_samps = int(args.window*fs)         
     samps_ndx = 0;  #first sample in window_ndx 
     max_window_ndx = nsamps / window_samps;
     bpms = numpy.zeros(max_window_ndx)
 
     #iterate through all windows
-    for window_ndx in range(0,int(max_window_ndx)):
+    for window_ndx in xrange(0,max_window_ndx):
 
         #get a new set of samples
+        #print n,":",len(bpms),":",max_window_ndx,":",fs,":",nsamps,":",samps_ndx
         data = samps[samps_ndx:samps_ndx+window_samps]
         if not ((len(data) % window_samps) == 0):
             raise AssertionError( str(len(data) ) ) 
         
-        bpm, correl_temp = bpm_detector(data,frame_per_second)
+        bpm, correl_temp = bpm_detector(data,fs)
         if bpm == None:
             continue
         bpms[window_ndx] = bpm
@@ -147,18 +151,19 @@ if __name__ == '__main__':
         n=n+1; #counter for debug...
 
     bpm = numpy.median(bpms)
-    print ('Completed.  Estimated Beats Per Minute:', bpm)
+    os.remove(args.filename+"converted")
+    print 'Completed.  Estimated Beats Per Minute:', bpm
     
-    #Adding bpm to the mp3 file back
     try:
-       audio = ID3(args.filename)
+        audio = ID3(args.filename)
     except ID3NoHeaderError:
-       audio = ID3()
-       audio.add(TBPM(encoding=3,text=str(bpm)))
-       audio.save(args.filename)
-    
-    n = range(0,int(len(correl)))
+        audio = ID3()
+        audio.add(TPE1(encoding=3, text=u'Artist'))
+        audio.add(TBPM(encoding=3, text=str(bpm)))
+        audio.save(args.filename)
+
+    n = range(0,len(correl))
     plt.plot(n,abs(correl)); 
     plt.show(False); #plot non-blocking
     time.sleep(10);
-    plt.close()
+    plt.close();
